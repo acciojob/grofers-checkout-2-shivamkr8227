b11ab30eb46e95983d293731de5650fa669db4dc
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 16:36:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-//your code here
-function calculateTotal() {
-    const prices = document.querySelectorAll("[data-ns-test='price']");
-    let total = 0;
+const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
 
-    prices.forEach((price) => {
-        total += parseFloat(price.textContent);
-    });
+let totalPrice = 0;
 
-    const grandTotalCell = document.querySelector("[data-ns-test='grandTotal']");
-    grandTotalCell.textContent = total.toFixed(2);
-}
-
-// Calculate the total initially
-calculateTotal();
-
-// Listen for changes in prices
-const prices = document.querySelectorAll("[data-ns-test='price']");
-prices.forEach((price) => {
-    price.addEventListener("input", calculateTotal);
+// iterate over the price elements and add their values to the total price
+priceElements.forEach((priceElement) => {
+  totalPrice += parseFloat(priceElement.textContent);
 });
+
+// get the grand total cell
+const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
+const grandTotal = parseFloat(grandTotalCell.textContent);
+
+// check if the total price equals the grand total
+if (totalPrice === grandTotal) {
+  console.log("Total prices match the grand total.");
+} else {
+  console.log("Total prices do not match the grand total.");
+}
