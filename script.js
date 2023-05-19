85f95dc17afcffde2b44a8133ba0647f99b6f58c
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 16:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,16 @@
 //your code here
+let table = document.querySelector("table");
+let priceList = document.querySelectorAll("[data-ns-test=price]");
+// console.log(priceList)
+let sum = 0;
+for(let i=0; i<priceList.length; i++){
+    sum += parseInt(priceList[i].innerText);
+}
+// console.log(sum)
 
-function calculateTotalPrice() {
-      var prices = document.querySelectorAll('[data-ns-test="price"]');
-      var total = 0;
-
-      // Iterate through each price element and sum the values
-      prices.forEach(function(priceElement) {
-        var price = parseInt(priceElement.innerText);
-        if(price){
-			total += price;
-		}
-          
-        
-      });
-
-      // Create a new row for displaying the total price
-      var table = document.getElementById('groceryTable');
-      var newRow = document.createElement('tr');
-      var totalCell = document.createElement('td');
-      totalCell.setAttribute('colspan', '2');
-      totalCell.textContent = 'Total Price: ' + total;
-      newRow.appendChild(totalCell);
-
-      // Set the data attribute for identifying the total element
-      totalCell.setAttribute('data-ns-test', 'grandTotal');
-
-      // Append the new row to the table
-      table.appendChild(newRow);
-    }
-
-    // Calculate the total price initially
-    calculateTotalPrice();
+let lastRow = document.createElement("tr");
+let tableData = document.createElement("td");
+tableData.setAttribute("data-ns-test", "grandTotal");
+tableData.innerText = sum;
+lastRow.appendChild(tableData);
+table.appendChild(lastRow);
