4b09a9f7f98b6007f83863ea16fb397d7cec30c6
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 16:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const priceElements = document.querySelectorAll('[data-ns-test="prices"]');
+//your code here
 
-let totalPrice = 0;
+function calculateTotalPrice() {
+      var prices = document.querySelectorAll('[data-ns-test="price"]');
+      var total = 0;
 
-// iterate over the price elements and add their values to the total price
-priceElements.forEach((priceElement) => {
-  totalPrice += parseFloat(priceElement.textContent);
-});
+      // Iterate through each price element and sum the values
+      prices.forEach(function(priceElement) {
+        var price = parseInt(priceElement.innerText);
+        if(price){
+			total += price;
+		}
+          
+        
+      });
 
-// get the grand total cell
-const grandTotalCell = document.querySelector('[data-ns-test="grandTotal"]');
-const grandTotal = parseFloat(grandTotalCell.textContent);
+      // Create a new row for displaying the total price
+      var table = document.getElementById('groceryTable');
+      var newRow = document.createElement('tr');
+      var totalCell = document.createElement('td');
+      totalCell.setAttribute('colspan', '2');
+      totalCell.textContent = 'Total Price: ' + total;
+      newRow.appendChild(totalCell);
 
-// check if the total price equals the grand total
-if (totalPrice === grandTotal) {
-  console.log("Total prices match the grand total.");
-} else {
-  console.log("Total prices do not match the grand total.");
-}
+      // Set the data attribute for identifying the total element
+      totalCell.setAttribute('data-ns-test', 'grandTotal');
+
+      // Append the new row to the table
+      table.appendChild(newRow);
+    }
+
+    // Calculate the total price initially
+    calculateTotalPrice();
