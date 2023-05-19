a1ceec5abf6c248996f0ab4ce020fb71b4e5af10
shivamkr8227 <63731531+shivamkr8227@users.noreply.github.com>
2023-05-19 16:22:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+function calculateTotal() {
+    const prices = document.querySelectorAll("[data-ns-test='prices']");
+    let total = 0;
+
+    prices.forEach((price) => {
+        total += parseFloat(price.textContent);
+    });
+
+    const grandTotalCell = document.querySelector("[data-ns-test='grandTotal']");
+    grandTotalCell.textContent = total.toFixed(2);
+}
+
+// Calculate the total initially
+calculateTotal();
+
+// Listen for changes in prices
+const prices = document.querySelectorAll("[data-ns-test='prices']");
+prices.forEach((price) => {
+    price.addEventListener("input", calculateTotal);
+});
